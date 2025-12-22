fb59e38627f0a7c5ace0aa66c4f03710a05db58f
Deuka <jitendrachoudhury682@gmail.com>
2025-12-22 12:43:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ function order(product) {
   const url = "https://wa.me/" + phone + "?text=" + encodeURIComponent(message);
   window.open(url, "_blank");
 }
+
+
+const searchIcon = document.getElementById("searchIcon");
+const searchBox = document.getElementById("searchBox");
+
+searchIcon.onclick = () => {
+  searchBox.style.display =
+    searchBox.style.display === "block" ? "none" : "block";
+};
+
+
+let cart = [];
+
+function addToCart(productName) {
+  cart.push(productName);
+  alert(productName + " added to cart");
+  console.log(cart);
+}
+
+let wishlist = [];
+
+function addToWishlist(item) {
+  wishlist.push(item);
+  alert("Added to wishlist");
+}
